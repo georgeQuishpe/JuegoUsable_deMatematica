bc346baf667dbce8077a8d63897b891290772bc6
George Quishpe <george.quishpe@epn.edu.ec>
2024-08-14 16:18:44 -0500

    fix: modificación de "alt" en botones del menu

--- src/menu.js
+++ src/menu.js
@@ -13,19 +13,19 @@ const Menu = () => {
   return (
     <div className="menu">
       <button className="menu-button" onClick={() => navigate('/PasarNivel1')}>
-        <img src={play}/>
+        <img src={play} alt="ícono jugar" />
         <span>Jugar</span>
       </button>
       <button className="menu-button" onClick={() => navigate('/Instrucciones')}>
-        <img src={instrucciones}/>
+        <img src={instrucciones} alt="ícono instrucciones" />
         <span>Instrucciones</span>
       </button>
       <button className="menu-button btn-material" onClick={() => navigate('/Material')}>
-        <img src={material} />
+        <img src={material} alt="ícono material de estudios" />
         <span>Material de estudios</span>
       </button>
       <button className="menu-button" onClick={() => navigate('/Configuracion')}>
-        <img src={settigs}/>
+        <img src={settigs} alt="ícono configuraciones" />
         <span>configuraciones</span>
       </button>
     </div>
